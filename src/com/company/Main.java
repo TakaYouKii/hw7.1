b69f2f9fecd5d3da40d657f6d5ebe90e9f29e0e3
Admin <avwro1708@gmail.com>
2021-07-29 20:22:22 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Magic magic = new Magic();
+        Medic medic = new Medic();
+        Warrior warrior = new Warrior();
+        Hero[] heroes = {magic,medic,warrior};
+        for (Hero hero: heroes){
+            hero.applySuperAbility("");
+        }
+
+
+    }
+}
